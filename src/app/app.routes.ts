4f3289f3c758399198f4abe71d2ca4384b0860a3
Abdelhafid ELAMAL <elamal.a980@ucd.ac.ma>
2026-07-31 23:07:24 +0100

    Add 2 component home and from and rounting

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { Home } from '../app/home/home';
+import { Store } from '../app/store/store';
+import { Form } from '../app/form/form';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path : 'home' , component : Home } ,
+    { path : 'store' , component : Store } ,
+    { path : 'form' , component : Form } 
+];
---
+++ src/app/form/form.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-form',
+  imports: [
+    RouterLink
+  ],
+  templateUrl: './form.html',
+  styleUrl: './form.css',
+  standalone : true
+
+})
+export class Form {
+  
+
+}
