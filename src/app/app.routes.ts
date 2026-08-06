bb562390558537a9ae2cc1697022d09c3afe6da8
Abdelhafid ELAMAL <elamal.a980@ucd.ac.ma>
2026-08-06 13:59:49 +0100

    resolve routing configuration and component imports

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,17 @@
 import { Routes } from '@angular/router';
-import { AboutComponent } from './about/about';
+import { AboutComponent  } from './about/about';
+import { HomeComponent  } from './home/home';
+import { ContactComponenet  } from './contact/contact';
+import { StoreComponenet  } from './store/store';
 
 export const routes: Routes = [
-  { path: '', redirectTo: 'home', pathMatch: 'full' },
-
+ 
   { path: 'about', component: AboutComponent },
+
+  { path: 'contact', component: ContactComponent },
+
+  { path: 'store', component: StoreComponent },
+
 
   { path: '**', redirectTo: 'about' }
 ];
